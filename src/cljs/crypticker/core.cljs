dc755bab88e575dd86cbbbd38db98334f0f82be2
Brandon Freitag <freitagbr@gmail.com>
2017-12-30 14:12:56 -0800

    good looking

--- src/cljs/crypticker/core.cljs
+++ src/cljs/crypticker/core.cljs
@@ -5,95 +5,108 @@
             [secretary.core   :as secretary :include-macros true]
             [accountant.core  :as accountant]
             [cljs-http.client :as http]
-            [socket.io]))
+            [goog.i18n.NumberFormat.Format])
+  (:import (goog.i18n NumberFormat)
+           (goog.i18n.NumberFormat Format)))
+
+;; -------------------------
+;; Utilities
+
+(defonce nff (NumberFormat. Format/CURRENCY))
+
+(defn- nf [n]
+  (.format nff (str n)))
+
+(defn- amt [cx]
+  (nf (js/parseFloat cx)))
 
 ;; -------------------------
 ;; Components
 
-(defonce timer (atom (js/Date.)))
-(defonce time-color (atom "#f34"))
-(defonce time-updater
-  (js/setInterval
-    #(reset! timer (js/Date.)) 1000))
-(defonce txs (atom '()))
-(defonce tx-focus (atom {}))
-(defonce tx-color (atom "#abc"))
-(defonce socket (js/io "https://blockexplorer.com/"))
+(defonce cxs (atom (sorted-map)))
 
-(defn subscribe
-  "subscribes to the transaction channel"
+(defn- assoc-new-cx
+  "associates new currency info to currency map"
+  [cxs cx]
+  (let [cxid (:currency_id cx)
+        new-price (:vwap cx)
+        old-price (get-in cxs [cxid :vwap])
+        cmp (if (some? old-price)
+              (compare new-price old-price)
+              0)]
+    (assoc cxs cxid (assoc cx :cmp cmp))))
+
+(defn get-market-info
+  "gets current market information"
   []
-  (.emit socket "subscribe" "inv"))
+  (go
+    (let [res (<! (http/get "https://marketapi-vdev.blockexplorer.com/tickers"
+                            {:with-credentials? false}))
+          tickers (:tickers (:body res))]
+      (if (= 200 (:status res))
+        (doall
+          (for [cx tickers]
+            (swap! cxs assoc-new-cx cx)))))))
 
-(defn add-transaction
-  "adds a transaction to the list of transactions"
-  [data]
-  (swap! txs (fn [txs tx]
-               (cons tx (if (>= (count txs) 5)
-                          (drop-last txs)
-                          txs)))
-         data))
+(defonce currency-update
+  (js/setInterval get-market-info 5000))
 
-(defn get-tx
-  "gets info about a transaction"
-  [txid]
-  (go (let [response (<! (http/get (str "https://blockexplorer.com/api/tx/" txid)
-                                   {:with-credentials? false}))]
-        (reset! tx-focus (:body response)))))
+(defn cx-name [cx]
+  [:div.cx-name (:currency_name cx)])
 
-(defn clock []
-  (let [time-str (-> @timer .toTimeString (clojure.string/split " ") first)]
-    [:div.example-clock
-     {:style {:color @time-color}}
-     time-str]))
+(defn cx-code [cx]
+  [:div.cx-code (:currency_code cx)])
 
-(defn transactions []
-  [:table
-   [:thead
-    [:tr
-     [:th "Hash"] [:th "Value"]]]
-   (into [:tbody]
-         (for [tx @txs
-               :let [txid (.-txid tx)
-                     value (.-valueOut tx)]]
-           ^{:key txid}
-           [:tr.tx {:on-click #(get-tx txid)}
-            [:td
-             [:pre
-              {:style {:color @tx-color}}
-              (subs txid 0 8)]]
-            [:td
-             [:pre value]]]))])
+(defn cx-handle-and-price [cx]
+  (let [cmp (:cmp cx)
+        price (amt (:vwap cx))]
+    [:div.row
+     [:div.group
+      [cx-name cx]
+      [(cond
+         (> cmp 0) :span.circle.green
+         (< cmp 0) :span.circle.red
+         :else :span.circle)]
+      [cx-code cx]]
+     (cond
+       (> cmp 0) [:div.group
+                  [:div.price.pos price]
+                  [:div.chev-up]]
+       (< cmp 0) [:div.group
+                  [:div.price.neg price]
+                  [:div.chev-down]]
+       :else [:div.group
+              [:div.price price]
+              [:div.chev-flat]])]))
 
-(defn focused-tx []
-  (if (> (count @tx-focus) 0)
-    (let [{:keys [valueIn valueOut txid size fees blockheight]} @tx-focus]
-      [:div
-       [:h4 "Value In"]
-       [:div valueIn]
-       [:h4 "Value Out"]
-       [:div valueOut]
-       [:h4 "Hash"]
-       [:pre txid]
-       [:h4 "Size"]
-       [:pre (str size " bytes")]
-       [:h4 "Fees"]
-       [:div fees]
-       [:h4 "Block Height"]
-       [:div blockheight]])
-    nil))
+(defn cx-market-cap [cx]
+  [:div.cap (amt (:market_cap cx))])
 
+(defn cx-volume-24h [cx]
+  [:div.vol (str (amt (:volume_24h cx)) " in 24h")])
+
+(defn cx-supply [cx]
+  [:div.supply (amt (:circulating_supply cx))])
+
+(defn market-info []
+  (into [:div.cxs]
+        (for [[cid cx] @cxs]
+          ^{:key cid}
+          [:div.cx
+           [cx-handle-and-price cx]
+           [:div.row
+            [cx-market-cap cx]
+            [cx-volume-24h cx]]
+           [:div.row
+            [cx-supply cx]]])))
 
 ;; -------------------------
 ;; Views
 
 (defn home-page []
-  [:div [:h2 "Welcome to crypticker"]
+  [:div [:h2 "Crypticker"]
    [:div
-    [:h3 "The time is now:"]
-    [clock]
-    [transactions]
-    [focused-tx]]
+    [market-info]]
    [:div [:a {:href "/about"} "go to about page"]]])
 
 (defn about-page []
@@ -129,10 +142,5 @@
      (fn [path]
        (secretary/locate-route path))})
   (accountant/dispatch-current!)
-
-  ; set up socket.io connection to blockexplorer
-  (doto socket
-    (.on "connect" subscribe)
-    (.on "tx" add-transaction))
-
+  (get-market-info)
   (mount-root))
